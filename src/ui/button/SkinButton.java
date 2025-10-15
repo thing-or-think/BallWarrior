265cd562176cbd1bc71e076b952190aa49a95045
thing-or-think <thinhabcdefgh2k6@gmail.com>
2025-10-15 10:05:14 +0700

    refactor(ui): replace raw skin fields with SkinData model in SkinButton

--- src/ui/button/SkinButton.java
+++ src/ui/button/SkinButton.java
@@ -1,6 +1,7 @@
 package ui.button;
 
 import core.ResourceLoader;
+import data.SkinData;
 import entity.Rarity;
 import ui.base.Button;
 
@@ -8,54 +9,47 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class SkinButton extends Button {
-    private final int id;
-    private final String name;
-    private final Rarity rarity;
-    private final int price;
-    private boolean isBought;
-    private final Color color;
-    private final BufferedImage img;
+    private SkinData skinData;
 
     private static final BufferedImage common = ResourceLoader.loadImg("assets/images/CommonBg.jpg");
     private static final BufferedImage rare = ResourceLoader.loadImg("assets/images/RareBg.jpg");
     private static final BufferedImage epic = ResourceLoader.loadImg("assets/images/EpicBg.jpg");
     private static final BufferedImage legendary = ResourceLoader.loadImg("assets/images/LegendaryBg.jpg");
 
-    public SkinButton(int id, String name, Rarity rarity, int price, boolean isBought, String imgPath,
-                      int x, int y, int width, int height, Runnable activity) {
-        super(name, null, x, y, width, height);
-        this.id = id;
-        this.name = name;
-        this.rarity = rarity;
-        this.price = price;
-        this.isBought = isBought;
-        this.color = Color.WHITE;
-        this.img = ResourceLoader.loadImg(imgPath);
+    public SkinButton(int x, int y, int width, int height, SkinData skinData) {
+        super(null, null, x, y, width, height);
+        this.skinData = skinData;
+    }
+
+    public SkinButton(int x, int y, int width, int height, SkinData skinData, Runnable activity) {
+        super(null, null, x, y, width, height);
+        this.activity = activity;
+        this.skinData = skinData;
     }
 
     @Override
     public void draw(Graphics2D g2) {
-        switch (rarity) {
+        switch (skinData.getRarity()) {
             case COMMON -> g2.drawImage(common, x, y, width, height, null);
             case RARE -> g2.drawImage(rare, x, y, width, height, null);
             case EPIC -> g2.drawImage(epic, x, y, width, height, null);
             case LEGENDARY -> g2.drawImage(legendary, x, y, width, height, null);
         }
 
-        if (img == null) {
-            g2.setColor(color != null ? color : Color.WHITE);
+        if (skinData.display.type == "color") {
+            g2.setColor(skinData.display.value != null ? color : Color.WHITE);
             g2.fillOval(x + width / 4, y + height / 4 - 10, width / 2, height / 2);
         } else {
-            g2.drawImage(img, x + width / 4, y + height / 4 - 10, width / 2, height / 2, null);
+            g2.drawImage(ResourceLoader.loadImg(skinData.display.value), x + width / 4, y + height / 4 - 10, width / 2, height / 2, null);
         }
 
-        if (!isBought) {
+        if (!skinData.isBought()) {
             g2.setColor(new Color(0, 0, 0, 150));
             g2.fillRect(x, y, width, height);
 
             g2.setColor(Color.YELLOW);
             g2.setFont(new Font("Serif", Font.BOLD, 18));
-            g2.drawString(price + " 💰", x + width / 3, y + height - 10);
+            g2.drawString(skinData.getPrice() + " 💰", x + width / 3, y + height - 10);
         }
 
         if (hovered) {
