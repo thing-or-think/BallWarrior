0c4db6f20b4d30326c9902092396b7ffbab70268
thing-or-think <thinhabcdefgh2k6@gmail.com>
2025-10-16 10:57:21 +0700

    refactor(data): replace int with AtomicInteger in Equipped for thread safety

--- src/data/Equipped.java
+++ src/data/Equipped.java
@@ -1,19 +1,29 @@
 package data;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Equipped {
-    private int ballId;
-    private int paddleId;
+    private AtomicInteger ballId;
+    private AtomicInteger paddleId;
 
     public Equipped(Equipped equipped) {
-        this.ballId = equipped.ballId;
-        this.paddleId = equipped.paddleId;
+        this.ballId = new AtomicInteger(equipped.ballId.get());
+        this.paddleId = new AtomicInteger(equipped.ballId.get());
     }
 
-    public int getBallId() { return ballId; }
-    public int getPaddleId() { return paddleId; }
+    public int getBallId() { return ballId.get(); }
+    public int getPaddleId() { return paddleId.get(); }
 
     public void setEquipped(Equipped equipped) {
         this.ballId = equipped.ballId;
         this.paddleId = equipped.paddleId;
+    }
+
+    public void setBallId(int ballId) {
+        this.ballId.set(ballId);
+    }
+
+    public void setPaddleId(int paddleId) {
+        this.paddleId.set(paddleId);
     }
 }
