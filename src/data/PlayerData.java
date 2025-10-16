c279dfef0e6de425bd8442ed9bb1ff8d95a779e4
thing-or-think <thinhabcdefgh2k6@gmail.com>
2025-10-16 10:56:18 +0700

    refactor(data): use AtomicInteger for coins and add equipped setters

--- src/data/PlayerData.java
+++ src/data/PlayerData.java
@@ -1,26 +1,36 @@
 package data;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class PlayerData {
-    private int coins;
+    private AtomicInteger coins;
     private Equipped equipped;
     private Inventory inventory;
 
 
     public PlayerData(PlayerData playerData) {
-        this.coins = playerData.coins;
-        equipped = new Equipped(playerData.equipped);
-        inventory = new Inventory(playerData.inventory);
+        this.coins = new AtomicInteger(playerData.coins.get());
+        this.equipped = new Equipped(playerData.equipped);
+        this.inventory = new Inventory(playerData.inventory);
     }
 
-    public int getCoins() { return coins; }
+    public int getCoins() { return coins.get(); }
     public void setCoins(int coins) {
-        this.coins = coins;
+        this.coins.set(coins);
     }
     public Equipped getEquipped() { return equipped; }
     public Inventory getInventory() { return inventory; }
 
+    public void setEquippedBall(int ballId) {
+        equipped.setBallId(ballId);
+    }
+
+    public void setEquippedPaddle(int paddleId) {
+        equipped.setPaddleId(paddleId);
+    }
+
     public void setPlayerData(PlayerData playerData) {
-        this.coins = playerData.coins;
+        this.coins.set(playerData.coins.get());
         equipped.setEquipped(playerData.equipped);
         inventory.setInventory(playerData.inventory);
     }
